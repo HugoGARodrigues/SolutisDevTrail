5b329d43f3a2697b837d3fd6e898f09c81a6a0fe
HugoGabriel <90066517+HugoGARodrigues@users.noreply.github.com>
2024-08-19 07:42:22 -0300

    Update Questao7.java

--- Lista1/Questao7.java
+++ Lista1/Questao7.java
@@ -6,9 +6,9 @@ public class DivisaoPor2Multiplo3{
         for (int i = 1; i<=100; i++){
             if (i % 3 == 0){
                 int multiplo3Int = i/2;
-                float multiplo3Float = (float)i/2;
+                double multiplo3Double = (double)i/2;
 
-                System.out.println("Número: " + i + ", Divisão por 2 e resultado inteiro: " + multiplo3Int + ", Divisão por 2 e resultado float: " + multiplo3Float);
+                System.out.println("Número: " + i + ", Divisão por 2 e resultado inteiro: " + multiplo3Int + ", Divisão por 2 e resultado double: " + multiplo3Double);
 
             }
         }
